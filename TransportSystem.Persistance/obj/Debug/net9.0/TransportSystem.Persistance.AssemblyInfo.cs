4b24d8110c445dee7d4e0b48b9139cff3df1a5fc
Gabriel Zimmer <gt3zimmer@gmail.com>
2025-02-15 19:53:59 -0300

    ?

--- TransportSystem.Persistance/obj/Debug/net9.0/TransportSystem.Persistance.AssemblyInfo.cs
+++ TransportSystem.Persistance/obj/Debug/net9.0/TransportSystem.Persistance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransportSystem.Persistance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a89c4e82c0b96f20ce944af0f15aff39ec316ec3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b76ec28027a4b263120d549be661cf570a48c3c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransportSystem.Persistance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransportSystem.Persistance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
